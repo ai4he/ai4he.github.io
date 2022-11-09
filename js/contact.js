cc5420181235c4746d67d5b624ea7fd6c5b63178
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2022-11-09 16:56:22 -0500

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -3,6 +3,13 @@ document.querySelector("#contact-form").addEventListener("submit",submitForm);
 function submitForm(e){
     e.preventDefault();
     console.log(123);
+    
+    let name= document.querySelector(".name").value;
+    let email= document.querySelector(".email").value;
+    let message= document.querySelector(".message").value;
+    console.log(name,email,message)
+
+
 
 }
 
