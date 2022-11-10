e0f813e37fa9b00a4b6c3ace88201bf64d9e742c
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2022-11-09 22:53:54 -0500

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,20 @@
-// Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDnJtRzH8A9oShfneVufo_w_m0W8zvy5UM",
-    authDomain: "haie-68cc6.firebaseapp.com",
-    projectId: "haie-68cc6",
-    storageBucket: "haie-68cc6.appspot.com",
-    messagingSenderId: "309164652738",
-    appId: "1:309164652738:web:37d9c3aa467678e969e35b"
-  };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnJtRzH8A9oShfneVufo_w_m0W8zvy5UM",
+  authDomain: "haie-68cc6.firebaseapp.com",
+  projectId: "haie-68cc6",
+  storageBucket: "haie-68cc6.appspot.com",
+  messagingSenderId: "309164652738",
+  appId: "1:309164652738:web:37d9c3aa467678e969e35b"
+};
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Refernece contactInfo collections
 let contactInfo = firebase.database().ref("infos");
