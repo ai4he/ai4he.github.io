69c7c33014eed9db91839857d94ce59fa7ea43da
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2022-11-09 23:13:27 -0500

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -33,7 +33,7 @@ function submitForm(e) {
 
   saveContactInfo(name, email, message);
 
-  document.querySelector(".contact-form").reset();
+  document.querySelector("#contact-form").reset();
 }
 
 // Save infos to Firebase
